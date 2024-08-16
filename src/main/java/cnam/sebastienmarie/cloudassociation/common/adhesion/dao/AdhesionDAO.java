a307e5e86e15935fb8c01091a62b42524711b360
Marie Sebastien <sebastien.marie07@gmail.com>
2024-08-16 13:50:04 +0200

    Commit ajout de la fonctionnalité liste adherents

--- src/main/java/cnam/sebastienmarie/cloudassociation/common/adhesion/dao/AdhesionDAO.java
+++ src/main/java/cnam/sebastienmarie/cloudassociation/common/adhesion/dao/AdhesionDAO.java
@@ -1,11 +1,16 @@
 package cnam.sebastienmarie.cloudassociation.common.adhesion.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import cnam.sebastienmarie.cloudassociation.common.adhesion.domain.Adherent;
+import cnam.sebastienmarie.cloudassociation.common.association.domain.Associations;
+import java.util.List;
+
 
 public interface AdhesionDAO extends JpaRepository<Adherent, String>{
 
     Adherent findByEmail(String email);
+
+    List<Adherent> findByAssociations(Associations associations);
     
 }
